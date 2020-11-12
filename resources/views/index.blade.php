ac4d6088cf1a1b8bcb1319f4db812af39a7d5055
Susana Piñero <susananzth@gmail.com>
2020-11-12 11:31:04 -0500

    Agregando fontawesome en vendor. Agregando íconos en vista de search

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,24 +13,44 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{asset('/vendor/fontawesome-free/css/all.min.css')}}">
         <link href="{{asset('/css/style.css')}}" rel="stylesheet">
     </head>
-    <body class="bg-cover">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+    <body class="">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">TORRE</a>
+            </div>
+        </nav>
+        <header class="masthead">
+          <div class="container">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endif
+            <!-- Outer Row -->
+            <div class="row justify-content-center">
+
+              <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                  <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                      <div class="col-lg-6 d-none d-lg-block bg-login-image">
+                        <i class="fas fa-briefcase"></i>
+                      </div>
+
+                      <div class="col-lg-6">
+                        <i class="far fa-id-badge"></i>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            @endif
-        </div>
+
+              </div>
+
+            </div>
+
+          </div>
+        </header>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     </body>
