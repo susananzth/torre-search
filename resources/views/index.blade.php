4004b9d8aa62ab60c7b76102092c06b446f15437
Susana Piñero <susananzth@gmail.com>
2020-11-12 12:52:00 -0500

    Añadiendo estilos en el index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,22 +3,21 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
         <title>torre search</title>
         <!-- Ícono -->
         <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}" />
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
         <!-- Styles -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <link rel="stylesheet" href="{{asset('/vendor/fontawesome-free/css/all.min.css')}}">
         <link href="{{asset('/css/style.css')}}" rel="stylesheet">
     </head>
     <body class="">
+        <!-- Nav -->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container">
+                <!-- Logo -->
                 <a class="navbar-brand js-scroll-trigger" href="#page-top">TORRE</a>
             </div>
         </nav>
@@ -30,16 +29,20 @@
 
               <div class="col-xl-10 col-lg-12 col-md-9">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card card-torre o-hidden border-0 my-5">
                   <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row">
-                      <div class="col-lg-6 d-none d-lg-block bg-login-image">
-                        <i class="fas fa-briefcase"></i>
+                      <div class="col-sm-12 col-md-6 text-center">
+                        <div class="icon-stack lift">
+                          <i class="fas fa-briefcase icon-header"></i>
+                        </div>
                       </div>
 
-                      <div class="col-lg-6">
-                        <i class="far fa-id-badge"></i>
+                      <div class="col-sm-12 col-md-6 text-center">
+                        <div class="icon-stack lift">
+                          <i class="fas fa-id-card-alt icon-header-2"></i>
+                        </div>
                       </div>
                     </div>
                   </div>
