343f0fd43ea84339b104355d51a0a96266c1b2c4
Susana Piñero <susananzth@gmail.com>
2020-11-12 13:06:48 -0500

    Dividiendo el inicio en layout. Agregando partials del search.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,60 +1,29 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>torre search</title>
-        <!-- Ícono -->
-        <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}" />
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <!-- Styles -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{asset('/vendor/fontawesome-free/css/all.min.css')}}">
-        <link href="{{asset('/css/style.css')}}" rel="stylesheet">
-    </head>
-    <body class="">
-        <!-- Nav -->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-            <div class="container">
-                <!-- Logo -->
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">TORRE</a>
+@extends('layouts.external')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-xl-10 col-lg-12 col-md-9">
+      <div class="card card-torre o-hidden border-0 my-5">
+        <div class="card-body p-0">
+          <!-- Row -->
+          <div class="row">
+            <!-- Card Opportunities -->
+            <div class="col-sm-12 col-md-6 text-center">
+              <a class="icon-stack lift" href="">
+                <i class="fas fa-briefcase icon-header"></i>
+              </a>
             </div>
-        </nav>
-        <header class="masthead">
-          <div class="container">
-
-            <!-- Outer Row -->
-            <div class="row justify-content-center">
-
-              <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card card-torre o-hidden border-0 my-5">
-                  <div class="card-body p-0">
-                    <!-- Nested Row within Card Body -->
-                    <div class="row">
-                      <div class="col-sm-12 col-md-6 text-center">
-                        <div class="icon-stack lift">
-                          <i class="fas fa-briefcase icon-header"></i>
-                        </div>
-                      </div>
-
-                      <div class="col-sm-12 col-md-6 text-center">
-                        <div class="icon-stack lift">
-                          <i class="fas fa-id-card-alt icon-header-2"></i>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-
+            <!-- Card People -->
+            <div class="col-sm-12 col-md-6 text-center">
+              <a class="icon-stack lift" href="">
+                <i class="fas fa-id-card-alt icon-header-2"></i>
+              </a>
             </div>
-
-          </div>
-        </header>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    </body>
-</html>
+          </div><!-- END Row -->
+        </div>
+      </div><!-- END Card-torre -->
+    </div>
+  </div>
+</div><!-- END Container -->
+@endsection
---
+++ resources/views/layouts/external.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>torre search</title>
+        <!-- Ícono -->
+        <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}" />
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{asset('/vendor/fontawesome-free/css/all.min.css')}}">
+        <link href="{{asset('/css/style.css')}}" rel="stylesheet">
+    </head>
+    <body class="">
+        <!-- Nav -->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container">
+                <!-- Logo -->
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">TORRE</a>
+            </div>
+        </nav>
+        <header class="masthead">
+          <!-- Call content -->
+          @yield('content')
+        </header>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    </body>
+</html>
